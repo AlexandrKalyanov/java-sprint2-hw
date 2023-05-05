62e62a7616c77c4fdfd9a681dcc43d57e39c11da
a.kalyanov <av.kalyanov@yandex.ru>
2023-05-05 09:57:17 +0300

    add parser

--- src/YearlyCsvParser.java
+++ src/YearlyCsvParser.java
@@ -6,33 +6,20 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
-public class YearlyReport {
-    public ArrayList<Report> reports = new ArrayList<>();
+public class YearlyCsvParser {
+    public List<YearlyReportRecord> reports = new ArrayList<>();
 
-    public YearlyReport(String path){
+    public YearlyCsvParser(String path){
         List<String> tokens = readFileContents(path);
         for (int i = 1; i < tokens.size(); i++) {
             String[] token = tokens.get(i).split(","); // token = "01,1593150,false
             int month = Integer.parseInt(token[0]);
             int amount = Integer.parseInt(token[1]);
             boolean isExpense = Boolean.parseBoolean(token[2]);
-            Report report = new Report(month,amount,isExpense);
-            this.reports.add(report);
+            YearlyReportRecord reportRecord = new YearlyReportRecord(month,amount,isExpense);
+            this.reports.add(reportRecord);
 
         }
-    }
-    void getInfoForYearReport(){ //  метод
-        HashMap<Boolean,Integer> yearReport = new HashMap<>();
-        for (Report report : reports) {
-            yearReport.put(report.isExpense, yearReport.getOrDefault( report.amount,0) + report.amount);
-        }
-        yearReport.forEach((key, value) -> {
-            if (key == true){
-                System.out.println("Доходы за год: " + value);
-            }else if (key == false) {
-                System.out.println("Расходы за год: " + value);
-            }
-        });
     }
     List<String> readFileContents(String path) {
         try {
--- src/YearlyReportRecord.java
+++ src/YearlyReportRecord.java
@@ -6,13 +6,15 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Report {
+public class YearlyReportRecord {
 
-    public int month;
-    public int amount;
-    public boolean isExpense;
+    private int month;
+    
 
-    public Report(int month, int amount, boolean isExpense) {
+    private int amount;
+    private boolean isExpense;
+
+    public YearlyReportRecord(int month, int amount, boolean isExpense) {
         this.month = month;
         this.amount = amount;
         this.isExpense = isExpense;
